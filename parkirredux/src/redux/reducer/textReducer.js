9cddbd4b914268f980c06aa99ffdbaa79fc37a94
Kartika Nirwana Silalahi <tikasilalahi8@gmail.com>
2019-12-03 09:31:05 +0700

    update

--- parkirredux/src/redux/reducer/textReducer.js
+++ parkirredux/src/redux/reducer/textReducer.js
@@ -3,13 +3,8 @@ const INITIAL_STATE=0
 export default (state=INITIAL_STATE, action)=>{
     switch(action.type){
         case JUMLAH:
-            var tampungkalimat=(action.payload).split(' ')
-            console.log(tampungkalimat)
-            var jumlah = 0
-            for(var i = 0; i < tampungkalimat.length; i++)
-                if(tampungkalimat[i] !== '' )jumlah++
-            console.log(jumlah)
-            return jumlah
+            var jumlahkata=((action.payload).split(' ').filter((val)=>val !== '')).length
+            return jumlahkata
         default :
             return state;
     }
